07cc260208f0c26854bdd35871cd6a746ed11e84
RAJI-ENSEIRB <oussama.raji@enseirb-matmeca.fr>
2025-12-18 11:35:29 +0100

    FONCTIONS UTILITAIRES

--- refactoring.js
+++ refactoring.js
@@ -14,3 +14,16 @@ const RANGES = {
 
 
 //FONCTIONS UTILITAIRES
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomChoice(arr) {
+    return arr[getRandomInt(0, arr.length - 1)];
+}
+
+function getRandomBoolean() {
+    return Math.random() < 0.5;
+}
+

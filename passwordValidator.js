16d1694abdc2d837af2814cbea5cc30a0b029462
RAJI-ENSEIRB <oussama.raji@enseirb-matmeca.fr>
2026-01-05 22:38:14 +0100

    password: implémentation complète en TDD

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,6 +1,7 @@
 function validatePassword(password) {
   if (password.length < 8) return false;
   if (!/[0-9]/.test(password)) return false;
+  if (!/[a-zA-Z]/.test(password)) return false;
   return true;
 }
 
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,4 +1,3 @@
-test("le mot de passe doit contenir au moins un chiffre", () => {
-  expect(validatePassword("abcdefgh")).toBe(false);
-  expect(validatePassword("abcd1234")).toBe(true);
+test("mot de passe invalide sans lettre", () => {
+  expect(validatePassword("12345678")).toBe(false);
 });

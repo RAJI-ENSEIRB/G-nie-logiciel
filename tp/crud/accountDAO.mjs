9339ff81ec76d43b1671b090604a17608a7f8f86
Meni33 <oussama.raji@enseirb-matmeca.fr>
2026-01-06 14:49:10 +0100

    ajouter un compte

--- tp/crud/accountDAO.mjs
+++ tp/crud/accountDAO.mjs
@@ -1,7 +1,10 @@
 import { ACCOUNT_LIST } from "./database.mjs";
 
 export const accountDAO = {
-  insertAccount(account) {},
+  insertAccount(account) {
+    ACCOUNT_LIST.push(account);
+    console.log("Contenu de la BDD :", ACCOUNT_LIST);
+  },
   retrieveAccountList() {},
   updateAccount(account) {},
   retrieveAccount(id) {},
--- tp/crud/client.mjs
+++ tp/crud/client.mjs
@@ -1,1 +1,6 @@
 import { accountService } from "./accountService.mjs";
+
+
+accountService.addAccount("RAJI", "Oussama");
+accountService.addAccount("RAJ", "mohamed");
+accountService.addAccount("Durand", "alice");
